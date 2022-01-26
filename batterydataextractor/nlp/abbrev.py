530cfdcd73fd4910c386eea3337da6b9f5b136d9
Shu Huang <sh2009@cam.ac.uk>
2022-01-26 23:54:11 +0800

    abbrev add spans

--- batterydataextractor/nlp/abbrev.py
+++ batterydataextractor/nlp/abbrev.py
@@ -17,32 +17,42 @@ class AbbreviationDetector(object):
     def __init__(self, model_name="en_abbreviation_detection_roberta_lar"):
         self.model = spacy.load(model_name)
 
-    def detect(self, tokens):
+    def detect_spans(self, tokens):
         doc = self.model(" ".join(tokens))
         entities = doc.ents
-        text_label = [(e.text, e.label_) for e in entities]
+        text_label = [(e.text, e.start, e.end, e.label_) for e in entities]
         g_list = [list(g) for k, g in itertools.groupby(text_label, key=lambda x: x[-1])]
-        new_tuples = [("".join(i), j[0]) for i, j in [zip(*i) for i in g_list]]
+        new_tuples = [(" ".join(i), j, h, k[0]) for i, j, h, k in [zip(*i) for i in g_list]]
         pairs = []
         for index, tuples in enumerate(new_tuples):
             if tuples[-1] == "LF":
                 lf_tokens = tuples[0].split(" ")
+                lf_spans = (tuples[1][0], tuples[2][-1])
                 left, right = index, index
                 # TODO: Need to optimise this logic
                 while True:
                     if right < len(new_tuples):
                         if new_tuples[right][-1] == "AC":
-                            abbrev_text = [new_tuples[right][0]]
+                            abbrev_spans = (new_tuples[right][1][0], new_tuples[right][2][-1])
+                            # abbrev_text = [new_tuples[right][0]]
                             break
                         right += 1
                     else:
                         if new_tuples[left][-1] == "AC" and left >= 0:
-                            abbrev_text = [new_tuples[left][0]]
+                            abbrev_spans = (new_tuples[left][1][0], new_tuples[left][2][-1])
+                            # abbrev_text = [new_tuples[left][0]]
                             break
                         left -= 1
-                pair = (abbrev_text, lf_tokens)
+                pair = (abbrev_spans, lf_spans)
                 pairs.append(pair)
         return pairs
+
+    def detect(self, tokens):
+        results = []
+        doc = self.model(" ".join(tokens))
+        for abbr_span, long_span in self.detect_spans(tokens):
+            results.append((doc[abbr_span[0]:abbr_span[1]].text.split(" "), doc[long_span[0]:long_span[1]].text.split(" ")))
+        return results
 
 
 class ChemAbbreviationDetector(AbbreviationDetector):

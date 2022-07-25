c51cffeb6c57ceb4e49a77657a38d3873db62c2e
ShuHuang <sh2009@cam.ac.uk>
2022-07-25 16:35:14 +0100

    pypi

--- batterydataextractor/__init__.py
+++ batterydataextractor/__init__.py
@@ -1,1 +1,11 @@
+"""
+batterydataextractor.__init__
+
+BatteryDataExtractor
+"""
+
+__version__ = "1.0.0"
+__author__ = 'Shu Huang'
+__credits__ = 'Molecular Engineering Group, Cavendish Laboratory'
+
 from .doc.document import Document
--- setup.py
+++ setup.py
@@ -6,45 +6,53 @@ setup.py
 BatteryDataExtractor setup
 author: Shu Huang (sh2009@cam.ac.uk)
 """
-import os
 from setuptools import setup, find_packages
 
 with open('LICENSE', encoding='utf-8') as f:
     license = f.read()
 
-with open('requirements.txt', encoding='utf-8') as f:
-    reqs = f.read()
-
-if os.path.exists('README.md'):
-    long_description = open('README.md').read()
-else:
-    long_description = '''BatteryDataExtractor is the first property-specific text-mining tool for auto-generating 
-    databases of materials and their property, device, and associated characteristics. The software has been 
-    constructed by embedding the BatteryBERT model.'''
-
 setup(
     name='batterydataextractor',
-    version='1.0.0',
+    version='1.2.0',
     author='Shu Huang',
     author_email='sh2009@cam.ac.uk',
     license=license,
     url='https://github.com/ShuHuang/batterydataextractor',
     packages=find_packages(),
     description='BatteryDataExtractor: battery-aware text-mining software embedded with BERT models',
-    long_description=long_description,
-    keywords='bde ',
+    long_description='''BatteryDataExtractor is the first property-specific text-mining tool for auto-generating 
+    databases of materials and their property, device, and associated characteristics. The software has been 
+    constructed by embedding the BatteryBERT model.''',
+    keywords='bde',
     zip_safe=False,
-    install_requires=reqs.strip().split('\n'),
+    install_requires=[
+        'selenium>=4.1.0',
+        'requests>=2.27.1',
+        'beautifulsoup4>=4.10.0',
+        'numpy>=1.22.0',
+        'torch>=1.11.0',
+        'torchvision>=0.12.0',
+        'torchaudio>=0.11.0',
+        'transformers>1.18.0',
+        'lxml>=4.7.1',
+        'cssselect>=1.1.0',
+        'appdirs>=1.4.4',
+        'scispacy>=0.4.0',
+        'spacy==3.0.7',
+        'six'
+    ],
+    dependency_links=[
+    'https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.4.0/en_core_sci_sm-0.4.0.tar.gz'
+    ],
     classifiers=[
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
         'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
         'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Cheminformatics',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
         'Topic :: Scientific/Engineering :: Chemistry',
     ],
 )
